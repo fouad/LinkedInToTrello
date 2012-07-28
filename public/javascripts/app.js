f2ee9d80e23b3739909088aa0368c8b5cd308527
Fouad Matin <fomatin@gmail.com>
2012-07-27 19:38:54 -0700

    fixed search when not authorized bug and undefined public profile bug

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -73,7 +73,9 @@
                                             console.log(person); 
                                             description = '';
                                             description += '#'+person.headline+'\n\n';
-                                            description += '**Profile Url**: '+person.publicProfileUrl+'\n\n';
+                                            if(person.publicProfileUrl) {
+                                                description += '**Profile Url**: '+person.publicProfileUrl+'\n\n';
+                                            }   
                                             if (person.threeCurrentPositions){
                                                 for(pos in person.threeCurrentPositions.values) {
                                                     description += '**Position**: '+person.threeCurrentPositions.values[pos].title+' at '+person.threeCurrentPositions.values[pos].company.name+'\n\n';
@@ -102,66 +104,78 @@
 
 
         $('#search').click(function(){
-            var profid = $('#profid').val();
-            if(profid == "") {
-                $('#profid').addClass('error');
-            } else {
-                var params = {};
-                var ext = profid.split(' ');
-                params['first-name'] = ext[0];
-                if(ext.length > 1) {
-                    params['last-name'] = ext[1];
-                } 
-                console.log(params);
-                IN.API.PeopleSearch()
-                    .fields(['firstName','lastName', 'id', 'publicProfileUrl', 'pictureUrl'])
-                    .params(params)
-                    .result(function(result){ 
-                    $('.profiles').html('');
-                    console.log(result);
-                    console.log(result.people.values);
-                    for(index in result.people.values) {
-                        var person = result.people.values[index];
-                        console.log(person);
-                        if(person.pictureUrl === undefined) {
-                            person.pictureUrl = 'https://www.linkedin.com/scds/common/u/img/icon/icon_no_photo_80x80.png'
-                        }
-                        var profile = '<div class="panel">';
-                        profile += '<div class="row">';
-                            profile += '<div class="two columns">';
-                                profile += '<img src="'+person.pictureUrl+'" />';
-                            profile += '</div>';
-                            profile += '<div class="ten columns">';
-                                profile += '<a href="'+person.publicProfileUrl+'" target="_blank"><h2>'+person.firstName+' '+person.lastName+'</h2></a>';
-                            profile += '</div>';
-                        profile += '</div>';
-                        profile += '<div class="row">';
-                            profile += '<div class="nine columns"></div>'
-                            profile += '<div class="three columns">';
-                                profile += '<a class="button addTrello" data-id="'+person.id+'">Add to Trello</a>';
-                            profile += '</div>';
-                        profile += '</div>';
-                        $('.profiles').append(profile);
-                    }
-                    $('.addTrello').click(function(e) {
-                        if(!$(this).hasClass('disabled')) {
-                            $(this).addClass('disabled');
-                            if(connectedToTrello) {
-                                // present options and add card to trello
-                                addCard(this);
-                            } else {
-                                Trello.authorize({
-                                    type: 'popup',
-                                    scope: { write: true, read: true },
-                                    success: clickAddTrello
-                                });
-                                $(this).removeClass('disabled');
+            if(IN.User.isAuthorized()) {
+                var profid = $('#profid').val();
+                if(profid == "") {
+                    $('#profid').addClass('error');
+                } else {
+                    var params = {};
+                    var ext = profid.split(' ');
+                    params['first-name'] = ext[0];
+                    if(ext.length > 1) {
+                        params['last-name'] = ext[1];
+                    } 
+                    console.log(params);
+                    IN.API.PeopleSearch()
+                        .fields(['firstName','lastName', 'id', 'publicProfileUrl', 'pictureUrl'])
+                        .params(params)
+                        .result(function(result){ 
+                        $('.profiles').html('');
+                        console.log(result);
+                        console.log(result.people.values);
+                        for(index in result.people.values) {
+                            var person = result.people.values[index];
+                            console.log(person);
+                            if(person.pictureUrl === undefined) {
+                                person.pictureUrl = 'https://www.linkedin.com/scds/common/u/img/icon/icon_no_photo_80x80.png'
                             }
+                            var profile = '<div class="panel">';
+                            profile += '<div class="row">';
+                                profile += '<div class="two columns">';
+                                    profile += '<img src="'+person.pictureUrl+'" />';
+                                profile += '</div>';
+                                if (person.publicProfileUrl) {
+                                    profile += '<div class="ten columns">';
+                                        profile += '<a href="'+person.publicProfileUrl+'" target="_blank"><h2>'+person.firstName+' '+person.lastName+'</h2></a>';
+                                    profile += '</div>';
+                                }
+                            profile += '</div>';
+                            profile += '<div class="row">';
+                                profile += '<div class="nine columns"></div>'
+                                profile += '<div class="three columns">';
+                                    profile += '<a class="button addTrello" data-id="'+person.id+'">Add to Trello</a>';
+                                profile += '</div>';
+                            profile += '</div>';
+                            $('.profiles').append(profile);
                         }
+                        $('.addTrello').click(function(e) {
+                            if(!$(this).hasClass('disabled')) {
+                                $(this).addClass('disabled');
+                                if(connectedToTrello) {
+                                    // present options and add card to trello
+                                    addCard(this);
+                                } else {
+                                    Trello.authorize({
+                                        type: 'popup',
+                                        scope: { write: true, read: true },
+                                        success: clickAddTrello
+                                    });
+                                    $(this).removeClass('disabled');
+                                }
+                            }
+                        });
                     });
-                });
+                } 
+            } else {
+                var sbutton = $('#search');
+                sbutton.attr('title', 'First you have to sign in with LinkedIn.');
+                sbutton.addClass('has-tip');
             }
         });
+    });
+
+    IN.Event.on(IN, 'auth', function() {
+        $('#search').removeClass('has-tip');
     });
 
 })(jQuery);
